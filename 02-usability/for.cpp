ae71b5e83451ec6da6062ce68db894cdc7b5815a
uyplayer <32848979@qq.com>
2025-02-13 11:01:32 +0800

    chapter 02

---
+++ 02-usability/for.cpp
@@ -0,0 +1,24 @@
+//
+// Created by uyplayer on 13/2/2025.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+
+int main()
+{
+
+    std::vector<int> vec = {1, 2, 3, 4};
+    for (auto element : vec)
+        std::cout << element << std::endl; // read only
+    for (auto &element : vec) {
+        element += 1;                      // writeable
+    }
+    for (auto element : vec)
+        std::cout << element << std::endl; // read only
+    return 0;
+}
